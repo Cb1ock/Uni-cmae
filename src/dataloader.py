chore: Update masking ratios for audio and video and fix some errors

src/dataloader.py
@@ -371,7 +371,7 @@ class AudiosetDataset(Dataset):
                     frames_list.append(new_frames)
 
             frames = torch.stack(frames_list, dim=0)
-
+            frames = frames.squeeze(0)
             if self.mode in ["test"]:
                 return frames
             else:

src/models/uni_cmae.py
@@ -287,8 +287,7 @@ class Uni_CMAE(nn.Module):
         x: [N, L, D], sequence
         """
         N, L, D = x.shape  # batch, length, dim
-        len_keep = int(L * (1 - mask_ratio))
-
+        len_keep = int(L * (1 - mask_ratio) + 0.5)
         noise = torch.rand(N, L, device=x.device)  # noise in [0, 1]
 
         # sort noise for each sample
@@ -464,7 +463,7 @@ class Uni_CMAE(nn.Module):
 
         return loss
 
-    def forward(self, audio, imgs, mask_ratio_a=0.75, mask_ratio_v=0.75, mae_loss_weight=1., contrast_loss_weight=0.01, mask_mode='unstructured'):
+    def forward(self, audio, imgs, mask_ratio_a=0.5, mask_ratio_v=0.9, mae_loss_weight=1., contrast_loss_weight=0.01, mask_mode='unstructured'):
         # latent is used for reconstruction (mae), latent_c_{a,v} are used for contrastive learning
         latent, mask_a, ids_restore_a, mask_v, ids_restore_v, latent_c_a, latent_c_v = self.forward_encoder(audio, imgs, mask_ratio_a, mask_ratio_v, mask_mode=mask_mode)
         # if mae loss is used

src/traintest_cavmae.py
@@ -76,7 +76,7 @@ def train(audio_model, train_loader, test_loader, args):
         print('---------------')
         print(datetime.datetime.now())
         print("current #epochs=%s, #steps=%s" % (epoch, global_step))
-        print('current masking ratio is {:.3f} for both modalities; audio mask mode {:s}'.format(args.masking_ratio, args.mask_mode))
+        print(f'current audio masking ratio is {args.masking_ratio_a}, current visual masking ratio is {args.masking_ratio_v}; audio mask mode {args.mask_mode}')
 
         for i, (a_input, v_input, _) in enumerate(train_loader):
 
@@ -88,8 +88,8 @@ def train(audio_model, train_loader, test_loader, args):
             per_sample_data_time.update((time.time() - end_time) / a_input.shape[0])
             dnn_start_time = time.time()
 
-            with autocast():
-                loss, loss_mae, loss_mae_a, loss_mae_v, loss_c, mask_a, mask_v, c_acc = audio_model(a_input, v_input, args.masking_ratio, args.masking_ratio, mae_loss_weight=args.mae_loss_weight, contrast_loss_weight=args.contrast_loss_weight, mask_mode=args.mask_mode)
+            with autocast():            # TODO：modify the mask ratio
+                loss, loss_mae, loss_mae_a, loss_mae_v, loss_c, mask_a, mask_v, c_acc = audio_model(a_input, v_input, args.masking_ratio_a, args.masking_ratio_v, mae_loss_weight=args.mae_loss_weight, contrast_loss_weight=args.contrast_loss_weight, mask_mode=args.mask_mode)
                 # this is due to for torch.nn.DataParallel, the output loss of 4 gpus won't be automatically averaged, need to be done manually
                 loss, loss_mae, loss_mae_a, loss_mae_v, loss_c, c_acc = loss.sum(), loss_mae.sum(), loss_mae_a.sum(), loss_mae_v.sum(), loss_c.sum(), c_acc.mean()
 
@@ -112,6 +112,8 @@ def train(audio_model, train_loader, test_loader, args):
             print_step = print_step or early_print_step
 
             if print_step and global_step != 0:
+                current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                print(f"Epoch {epoch}/{args.n_epochs} - Step {i}/{len(train_loader)} - Time: {current_time}")
                 print('Epoch: [{0}][{1}/{2}]\t'
                   'Per Sample Total Time {per_sample_time.avg:.5f}\t'
                   'Per Sample Data Time {per_sample_data_time.avg:.5f}\t'
@@ -201,7 +203,7 @@ def validate(audio_model, val_loader, args):
             a_input = a_input.to(device)
             v_input = v_input.to(device)
             with autocast():
-                loss, loss_mae, loss_mae_a, loss_mae_v, loss_c, mask_a, mask_v, c_acc = audio_model(a_input, v_input, args.masking_ratio, args.masking_ratio, mae_loss_weight=args.mae_loss_weight, contrast_loss_weight=args.contrast_loss_weight, mask_mode=args.mask_mode)
+                loss, loss_mae, loss_mae_a, loss_mae_v, loss_c, mask_a, mask_v, c_acc = audio_model(a_input, v_input, args.masking_ratio_a, args.masking_ratio_v, mae_loss_weight=args.mae_loss_weight, contrast_loss_weight=args.contrast_loss_weight, mask_mode=args.mask_mode)
                 loss, loss_mae, loss_mae_a, loss_mae_v, loss_c, c_acc = loss.sum(), loss_mae.sum(), loss_mae_a.sum(), loss_mae_v.sum(), loss_c.sum(), c_acc.mean()
             A_loss.append(loss.to('cpu').detach())
             A_loss_mae.append(loss_mae.to('cpu').detach())