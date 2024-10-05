f86f997617938c0a19e5cade0475ff090e829600
Cb1ock <ch3197@qq.com>
2024-10-06 02:04:49 +0800

    delete requirements.txt

--- egs/MAFW/run_uni-cmae_ft.sh
+++ egs/MAFW/run_uni-cmae_ft.sh
@@ -4,7 +4,7 @@
 
 export TORCH_HOME=../../pretrained_models
 
-model=uni-cmae-ft # or uni_cmae_ablation
+model=uni_cmae_ablation # or uni_cmae_ablation
 ftmode=multimodal # multimodal or audioonly or videoonly
 
 # you can replace with any checkpoint you want, but by default, we use cav-mae-scale++
@@ -18,10 +18,10 @@ lr=1e-5
 lr_scheduler=cosine
 warmup_epochs=2.5
 
-epoch=20
+epoch=25
 wa=True
 wa_start=10
-wa_end=20
+wa_end=25
 tr_pos=True
 
 dataset_mean=-5.081
@@ -32,7 +32,7 @@ noise=True
 freqm=48
 timem=192
 mixup=0.2
-batch_size=32
+batch_size=12
 label_smooth=0.3
 drop_path=0
 num_tests=10
@@ -40,7 +40,7 @@ num_tests=10
 dataset=MAFW
 label_csv=/home/hao/Project/uni-cmae/egs/MAFW/class_labels_indices.csv
 
-pretrain_path="/home/hao/Project/uni-cmae/pretrained_model/UniCmae_uninorm_bs50_PixnormTrue_tpTrue_c0.01_0.9_0.5_1e-4_16frame.pth"
+pretrain_path="/home/hao/Project/uni-cmae/pretrained_model/UniCmae_2LayerFusion_bs50_PixnormTrue_tpTrue_c0.01_0.9_0.5_1e-4_16frame.pth"
 
 for fold in {1..5}
 do
@@ -48,10 +48,10 @@ do
     val_data=/home/hao/Project/uni-cmae/egs/MAFW/cross_validation/fold_${fold}_val.json
     data_test=/home/hao/Project/uni-cmae/egs/MAFW/cross_validation/fold_${fold}_val.json
     weight_file=/home/hao/Project/uni-cmae/egs/MAFW/cross_validation/fold_${fold}_train_weights.npy
-    exp_dir=./exp/new-weights_1_full-${model}-${balance}-${lr}-${optimizer}-bs${batch_size}--${ftmode}-mx${mixup}-dp${drop_path}-fz${freeze_base}-h${head_lr}-war/fold${fold}
+    exp_dir=./exp/2LayerFusion-${model}-${balance}-${lr}-${optimizer}-bs${batch_size}--${ftmode}-mx${mixup}-dp${drop_path}-fz${freeze_base}-h${head_lr}-war/fold${fold}
     mkdir -p $exp_dir
 
-    CUDA_CACHE_DISABLE=1 CUDA_VISIBLE_DEVICES=1,2,4,6 python -W ignore ../../src/run_unicmae_ft.py --model ${model} --dataset ${dataset} \
+    CUDA_CACHE_DISABLE=1 CUDA_VISIBLE_DEVICES=1,2,3,4,5,6, python -W ignore ../../src/run_unicmae_ft.py --model ${model} --dataset ${dataset} \
     --data_train ${tr_data} --data-val ${val_data} --data-test ${data_test} --exp-dir $exp_dir \
     --label_csv ${label_csv} --n_class 11 --num_tests ${num_tests} \
     --lr $lr --n-epochs ${epoch} --batch-size $batch_size --save_model True --tr_pos ${tr_pos} \
@@ -63,5 +63,5 @@ do
     --wa ${wa} --wa_start ${wa_start} --wa_end ${wa_end} --lr_scheduler ${lr_scheduler} \
     --pretrain_path ${pretrain_path} --ftmode ${ftmode} --debug False \
     --freeze_base ${freeze_base} --head_lr ${head_lr} \
-    --num-workers 32 >> $exp_dir/train.log 2>&1
+    --num-workers 32 > $exp_dir/train.log 2>&1
 done
